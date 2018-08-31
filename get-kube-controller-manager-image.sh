247980d611d188ebf04eb8716e9472006f2282f5
galaxyobe <galaxyobe@qq.com>
2018-08-31 19:07:01 +0800

    v1.11.2

---
+++ get-kube-controller-manager-image.sh
@@ -0,0 +1,3 @@
+docker pull kubernets/kube-controller-manager-amd64:v1.11.2
+docker tag kubernets/kube-controller-manager-amd64:v1.11.2 gcr.io/google-containers/kube-controller-manager-amd64:v1.11.2 
+docker rmi kubernets/kube-controller-manager-amd64:v1.11.2
